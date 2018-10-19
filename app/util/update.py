8ba2b40b459e610813a4e144faaa703e0cb0a30e
InfiniteXyy <infinitexyy@gmail.com>
2018-10-19 18:06:53 +0800

    update update

--- app/util/update.py
+++ app/util/update.py
@@ -7,16 +7,15 @@ books = my_db["books"]
 
 
 def refresh_data(spider: Spider):
-    print("正在更新", spider.book_name)
     new_articles = []
     chapter_list = spider.get_article_list()
-    print("线上文章：", len(chapter_list))
     cur_documents = list(chapters.find({"book_url": spider.book_url}, {"href": True}))
-    print("已有文章：", len(cur_documents))
+    print("{}: 线上文章 {} - 已有文章 {}".format(spider.book_name, len(chapter_list), len(cur_documents)))
+
     for index, i in enumerate(chapter_list):
         if len(list(filter(lambda x: x["href"] == i["href"], cur_documents))) == 0:
             i['content'] = spider.get_content(i['href'])
-            print("增加到暂存区： {} ".format(i["title"]))
+            print("增加： {} ".format(i["title"]))
             i['index'] = index + 1  # set article index
             i['book'] = spider.book_name
             i['book_url'] = spider.book_url
@@ -28,9 +27,3 @@ def refresh_data(spider: Spider):
         print("更新完成！共 {} 章".format(len(new_articles)))
         books.update_one({"url": spider.book_url},
                          {"$set": {"info": new_articles[-1]["title"], "update_time": time.time()}})
-    print("chapter amount:", chapters.estimated_document_count())
-
-
-if __name__ == '__main__':
-    refresh_data(Jianling())
-    refresh_data(Xieshen())
--- manage.py
+++ manage.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import time
+import sys
+import io
 from app import app
 from app.util.update import refresh_data
 from app.util.spider.jianling import Jianling
@@ -11,13 +15,17 @@ def switch_command(type):
     if type == "run":
         app.run("0.0.0.0", 5000, True)
     elif type == "refresh":
+        print("++++++++++++++++++++++++++")
+        print(datetime.fromtimestamp(time.time()).strftime("%m月%d日 %H:%M:%S"))
         for i in refresh_list:
             refresh_data(i)
 
 
 if __name__ == '__main__':
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
     if len(sys.argv) == 2:
         arg = sys.argv[1]
         switch_command(arg)
     else:
-        print("错误的参数！")
+        print("参数错误")
